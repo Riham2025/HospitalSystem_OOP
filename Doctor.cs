c01b0e00c28887ae9575e0e81c38a648ca10a425
riham <riham91.alsiyabi@gmail.com>
2025-07-22 15:06:30 +0400

    public Doctor(int id, string name, int age, string specialization) // Overloaded constructor for Doctor class

--- Doctor.cs
+++ Doctor.cs
@@ -11,10 +11,10 @@ namespace HospitalSystem_OOP
         private string specialization; //specialization of the doctor
         private List<DateTime> availableAppointments; // list of available appointments for the doctor
 
-        public string Specialization { get => specialization; set => specialization = value; }
-        public List<DateTime> AvailableAppointments { get => availableAppointments; set => availableAppointments = value; }
+        public string Specialization { get => specialization; set => specialization = value; } //specialization property
+        public List<DateTime> AvailableAppointments { get => availableAppointments; set => availableAppointments = value; } // list of available appointments property
 
-        public Doctor(int id, string name, int age, string specialization)
+        public Doctor(int id, string name, int age, string specialization) // Overloaded constructor for Doctor class
             : base(id, name, age)
         {
             this.specialization = specialization;
