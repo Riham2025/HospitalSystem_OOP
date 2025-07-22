f9bbe456584cbcbd57ad232af746baa515512b13
riham <riham91.alsiyabi@gmail.com>
2025-07-22 15:04:32 +0400

    Doctor class inherits from Person

--- Doctor.cs
+++ Doctor.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace HospitalSystem_OOP
 {
-    class Doctor
+    class Doctor : Person //Doctor class inherits from Person
     {
+        private string specialization;
+        private List<DateTime> availableAppointments;
+
+        public string Specialization { get => specialization; set => specialization = value; }
+        public List<DateTime> AvailableAppointments { get => availableAppointments; set => availableAppointments = value; }
+
+        public Doctor(int id, string name, int age, string specialization)
+            : base(id, name, age)
+        {
+            this.specialization = specialization;
+            this.availableAppointments = new List<DateTime>();
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Doctor: {Name}, Age: {Age}, Specialization: {specialization}");
+        }
     }
 }
