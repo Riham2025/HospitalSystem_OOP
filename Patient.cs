f33e2caf6d12ad69a6d7e128f08dd600f2344055
riham <riham91.alsiyabi@gmail.com>
2025-07-22 15:11:50 +0400

    public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; } // phone number property

--- Patient.cs
+++ Patient.cs
@@ -9,7 +9,7 @@ namespace HospitalSystem_OOP
     class Patient : Person //Patient class inherits from Person
     {
         private string phoneNumber; // phone number of the patient
-        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; } // phone number property  
 
         public Patient(int id, string name, int age, string phoneNumber)
             : base(id, name, age)
