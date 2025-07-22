d951e62b738fe24242725f78f3c4ccf257415acd
riham <riham91.alsiyabi@gmail.com>
2025-07-22 15:11:05 +0400

    Patient class inherits from Person

--- Patient.cs
+++ Patient.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace HospitalSystem_OOP
 {
-    class Patient
+    class Patient : Person //Patient class inherits from Person
     {
+        private string phoneNumber;
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        public Patient(int id, string name, int age, string phoneNumber)
+            : base(id, name, age)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Patient: {Name}, Age: {Age}, Phone: {phoneNumber}");
+        }
     }
 }
