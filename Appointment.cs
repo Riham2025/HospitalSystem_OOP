0cbf9f0252a63925d9528591f25289375c394a70
riham <riham91.alsiyabi@gmail.com>
2025-07-22 15:13:47 +0400

    Properties for Appointment class

---
+++ Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem_OOP
+{
+    class Appointment
+    {
+        // Properties for Appointment class
+        private int appointmentId;
+        private Doctor doctor;
+        private Patient patient;
+        private DateTime appointmentDate;
+
+        public int AppointmentId { get => appointmentId; set => appointmentId = value; }
+        public Doctor Doctor { get => doctor; set => doctor = value; }
+        public Patient Patient { get => patient; set => patient = value; }
+        public DateTime AppointmentDate { get => appointmentDate; set => appointmentDate = value; }
+
+    }
+}
