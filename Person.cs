2ad65c95acb8da27571a31a9fe19f057f7520f6e
riham <riham91.alsiyabi@gmail.com>
2025-07-22 14:56:47 +0400

    set the Default constructor

--- Person.cs
+++ Person.cs
@@ -8,12 +8,23 @@ namespace HospitalSystem_OOP
 {
     class Person
     {
+        // Fields
         private int id;
         private string name;
         private int age;
 
-
+        // Properties
         public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        // Default constructor
+        public Person(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
 
     }
 }
